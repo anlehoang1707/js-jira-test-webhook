359fa7da29e11a57cde5c3ef07b307e2c480326d
leanprojectmanager <an.lehoang1707@gmail.com>
2024-12-30 13:40:44 +0700

    Update jira-webhook.js

--- netlify/functions/jira-webhook.js
+++ netlify/functions/jira-webhook.js
@@ -8,7 +8,8 @@ app.use(express.json());
 app.post("/webhook", (req, res) => {
   console.log("Request received:");
   console.log("Headers:", req.headers);
-  console.log("Body:", req.body);
+  console.log("Body as JSON:", JSON.stringify(req.body, null, 2)); // Pretty-printed JSON
+
 
   res.status(200).json({ message: "Webhook received successfully" });
 });
