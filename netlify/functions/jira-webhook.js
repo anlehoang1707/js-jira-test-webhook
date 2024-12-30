e089c903d896407c24bd534f0646cad655707773
leanprojectmanager <an.lehoang1707@gmail.com>
2024-12-30 12:29:36 +0700

    edit netlify.toml for redirecting

--- netlify/functions/jira-webhook.js
+++ netlify/functions/jira-webhook.js
@@ -3,20 +3,14 @@ const serverless = require("serverless-http");
 
 const app = express();
 
-// Middleware to parse JSON data
 app.use(express.json());
 
-// Webhook route
 app.post("/webhook", (req, res) => {
-  const webhookData = req.body;
+  console.log("Request received:");
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
 
-  // Log the data for debugging
-  console.log("Webhook received:", webhookData);
-
-  // Respond back to Jira
-  res.status(200).json({
-    message: "Webhook received successfully",
-  });
+  res.status(200).json({ message: "Webhook received successfully" });
 });
 
 module.exports.handler = serverless(app);
